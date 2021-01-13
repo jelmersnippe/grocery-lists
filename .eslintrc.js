43362eda1fb3d971af74f89bc88316b7b385f338
jelmersnippe <jelmersnippe@hotmail.com>
2021-01-13 23:25:33 +0100

    Ignore 'react-hooks/exhaustive-deps' linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     extends: '@react-native-community',
     rules: {
         'prettier/prettier': 0,
+        'react-hooks/exhaustive-deps': 0,
         'indent': ['error', 4, {'SwitchCase': 1}],
         'comma-dangle': ['error', 'never'],
         'quotes': ['error', 'single'],
