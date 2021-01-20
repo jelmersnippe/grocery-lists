f6e36fd6671496fe3d59a7e4a74cdbc0b7e79893
jelmersnippe <jelmersnippe@hotmail.com>
2021-01-20 23:50:49 +0100

    Add basic view logic + UserModal to GroupDetails screen & only subscribe to groups you are part of

--- src/firestore/groupActions.ts
+++ src/firestore/groupActions.ts
@@ -1,20 +1,43 @@
 import firestore from '@react-native-firebase/firestore';
-import {FirestoreGroup} from './types';
+import {FirestoreGroup, FirestoreList, FirestoreUserUid} from './types';
 import {addGroup} from '../reducers/groups/actions';
 import {store} from '../config/store';
 
 const subscribeToUpdates = () => {
-    return firestore().collection('groups').onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((documentSnapshot) => {
-            const data = documentSnapshot.data() as FirestoreGroup;
-            const id = documentSnapshot.id;
-            store.dispatch(addGroup({id: id, group: data}));
+    const currentUserUid = store.getState().user.uid;
+
+    return firestore().collection('groups')
+        .where('users', 'array-contains', currentUserUid)
+        .onSnapshot((querySnapshot) => {
+            querySnapshot.forEach((documentSnapshot) => {
+                const data = documentSnapshot.data() as FirestoreGroup;
+                const id = documentSnapshot.id;
+                store.dispatch(addGroup({id: id, group: data}));
+            });
+        });
+};
+
+const addUsersToFirestoreGroup = async (groupId: string, usersToAdd: Array<FirestoreUserUid>, usersToRemove: Array<FirestoreUserUid>) => {
+    const groupUsersRef = firestore().doc(`groups/${groupId}`);
+
+    return firestore().runTransaction(async (transaction) => {
+        const listUsersSnapshot = await transaction.get(groupUsersRef);
+
+        if (!listUsersSnapshot.exists) {
+            throw 'Group users don\'t exists';
+        }
+
+        const listData = listUsersSnapshot.data() as FirestoreList;
+        const newListUsers = listData.users.filter((user) => !usersToRemove.includes(user));
+        await transaction.update(groupUsersRef, {
+            users: [...newListUsers, ...usersToAdd]
         });
     });
 };
 
 const firestoreGroupActions = {
-    subscribeToUpdates
+    subscribeToUpdates,
+    addUsersToFirestoreGroup
 };
 
 export default firestoreGroupActions;
--- src/screens/GroupDetails/index.tsx
+++ src/screens/GroupDetails/index.tsx
@@ -1,19 +1,69 @@
-import React, {FunctionComponent} from 'react';
-import {Text, View} from 'react-native';
+import React, {FunctionComponent, useEffect, useState} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {Props} from './props';
 import styles from './styles';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../reducers';
 import FullSizeLoader from '../../components/FullSizeLoader';
+import {setOverlay, useOverlayData} from '@jelmersnippe/flexible-overlays';
+import UserModal from '../../components/UserModal';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useTranslation} from 'react-i18next';
+import firestoreGroupActions from '../../firestore/groupActions';
+import {UserInfo} from '../../reducers/userCache/types';
+import firestoreUserActions from '../../firestore/userActions';
+import {capitalize} from '../../utils/capitalize';
 
 const GroupDetails: FunctionComponent<Props> = ({route}) => {
     const {id} = route.params;
     const selectedGroup = useSelector((rootState: RootState) => rootState.groups.hasOwnProperty(id) ? rootState.groups[id] : undefined);
+    const [users, setUsers] = useState<Array<UserInfo>>([]);
+    const {dispatch} = useOverlayData();
+    const {t} = useTranslation();
+
+    useEffect(() => {
+        (async () => {
+            if (selectedGroup?.users) {
+                const newUsers: Array<UserInfo> = [];
+                for (const user of selectedGroup.users) {
+                    const foundUser = await firestoreUserActions.getByUid(user);
+                    if (foundUser) {
+                        newUsers.push(foundUser);
+                    }
+                }
+                setUsers(newUsers);
+            }
+        })();
+    }, [selectedGroup?.users]);
 
     return (
         selectedGroup ?
             <View style={styles.container}>
                 <Text style={styles.title}>{selectedGroup.name}</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        dispatch(setOverlay({
+                            title: t('users'),
+                            content: <UserModal
+                                saveAction={async (usersToAdd, usersToRemove) => await firestoreGroupActions.addUsersToFirestoreGroup(id, usersToAdd, usersToRemove)}
+                                initialUsers={selectedGroup.users}
+                            />,
+                            wrapperStyle: {
+                                width: '80%',
+                                height: '80%'
+                            }
+                        }));
+                    }}
+                >
+                    <Icon name={'people'} size={36} color={'black'}/>
+                </TouchableOpacity>
+                {
+                    users.map((user) => {
+                        return (
+                            <Text>{capitalize(user.name)}</Text>
+                        );
+                    })
+                }
             </View>
             : <FullSizeLoader size={100} color={'black'}/>
     );
