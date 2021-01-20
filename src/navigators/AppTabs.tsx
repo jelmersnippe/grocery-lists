685f07c1841a1f584fa899198490f5dd9f377191
jelmersnippe <jelmersnippe@hotmail.com>
2021-01-20 23:47:18 +0100

    Remove Search screen

--- src/navigators/AppTabs.tsx
+++ src/navigators/AppTabs.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Search from '../screens/Search';
 import ListStack from './ListStack';
 import GroupStack from './GroupStack';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -10,7 +9,6 @@ import {Text} from 'react-native';
 export type AppTabsParamList = {
     Lists: undefined;
     Groups: undefined;
-    Search: undefined;
 }
 
 const Tabs = createBottomTabNavigator<AppTabsParamList>();
@@ -29,8 +27,6 @@ const AppTabs = () => {
                             return <Icon name={'people'} size={30} color={color}/>;
                         case 'Lists':
                             return <Icon name={'list'} size={30} color={color}/>;
-                        case 'Search':
-                            return <Icon name={'search'} size={30} color={color}/>;
                     }
                 },
                 tabBarLabel: ({focused}) => {
@@ -45,7 +41,6 @@ const AppTabs = () => {
         >
             <Tabs.Screen name='Groups' component={GroupStack}/>
             <Tabs.Screen name='Lists' component={ListStack}/>
-            <Tabs.Screen name='Search' component={Search}/>
         </Tabs.Navigator>
     );
 };
--- src/screens/Search/props.ts
+++
@@ -1,4 +0,0 @@
-import {AppTabsParamList} from '../../navigators/AppTabs';
-import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
-
-export type Props = BottomTabScreenProps<AppTabsParamList, 'Search'>;
