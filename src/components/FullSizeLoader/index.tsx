deac68720f6fe8a2d6aec72c62bf253a65ace150
jelmersnippe <jelmersnippe@hotmail.com>
2021-01-22 23:00:30 +0100

    Add default values to FullSizeLoader and show go back button after 5 seconds

--- src/components/FullSizeLoader/index.tsx
+++ src/components/FullSizeLoader/index.tsx
@@ -1,12 +1,43 @@
-import {ActivityIndicator, View} from 'react-native';
-import React, {FunctionComponent} from 'react';
+import {ActivityIndicator, Text, View} from 'react-native';
+import React, {FunctionComponent, useEffect, useState} from 'react';
 import {Props} from './props';
 import styles from './styles';
+import Button from '../Button';
+import {resetOverlay, useOverlayData} from '@jelmersnippe/flexible-overlays';
+import {useNavigation} from '@react-navigation/native';
 
-const FullSizeLoader: FunctionComponent<Props> = ({size, color}) => {
+const FullSizeLoader: FunctionComponent<Props> = ({size = 100, color = 'black'}) => {
+    const [showButton, setShowButton] = useState(false);
+    const {state, dispatch} = useOverlayData();
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShowButton(true);
+        }, 5000);
+    }, []);
+
+    const goBack = () => {
+        if (state?.isVisible) {
+            dispatch(resetOverlay());
+        } else {
+            navigation.goBack();
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <ActivityIndicator size={size} color={color}/>
+            {
+                showButton ?
+                    <>
+                        <Text>Something went wrong</Text>
+                        <Button
+                            text={'Go back'}
+                            onPress={() => goBack()}
+                        />
+                    </>
+                    : <ActivityIndicator size={size} color={color}/>
+            }
         </View>
     );
 };
